3cbd45b9fa1460b42009642cedff5b1d0d0552b5
Junho <51792037+ojh050118@users.noreply.github.com>
2023-03-19 22:11:46 +0900

    Initial commit

---
+++ RemoteAudioServer/MainWindow.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RemoteAudioServer;
+
+partial class MainWindow
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.DeviceInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DeviceInfo
+            // 
+            this.DeviceInfo.AutoSize = true;
+            this.DeviceInfo.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DeviceInfo.Location = new System.Drawing.Point(12, 9);
+            this.DeviceInfo.Name = "DeviceInfo";
+            this.DeviceInfo.Size = new System.Drawing.Size(140, 45);
+            this.DeviceInfo.TabIndex = 0;
+            this.DeviceInfo.Text = "PC 정보:";
+            this.DeviceInfo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.DeviceInfo);
+            this.Name = "MainWindow";
+            this.Text = "Remote Audio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private Label DeviceInfo;
+}
---
+++ RemoteAudioServer/MainWindow.cs
@@ -0,0 +1,17 @@
+namespace RemoteAudioServer;
+
+public partial class MainWindow : Form
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DeviceInfo.Text = $"PC 정보: {Utils.DeviceInfo.DeviceName}, {Utils.DeviceInfo.OS}";
+
+        ToolTip toolTip = new ToolTip();
+        toolTip.SetToolTip(DeviceInfo, "클라이언트에서 컴퓨터를 구분할때 사용합니다.");
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+    }
+}
