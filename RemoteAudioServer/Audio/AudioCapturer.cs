08e0fbb8404ebf9b8d9f8e8613971043dee329e1
Junho <51792037+ojh050118@users.noreply.github.com>
2023-03-19 22:33:10 +0900

    오디오 캡처 최소 구현

---
+++ RemoteAudioServer/Audio/AudioCapturer.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteAudioServer.Audio
+{
+    public class AudioCapturer : IDisposable
+    {
+        private readonly WasapiLoopbackCapture waveIn;
+        private readonly  WaveFormat format;
+
+        public AudioCapturer()
+        {
+            waveIn = new WasapiLoopbackCapture();
+            format = waveIn.WaveFormat;
+            waveIn.DataAvailable += onDavaAvailable;
+        }
+
+        public void Start() => waveIn.StartRecording();
+
+        public void Stop() => waveIn.StopRecording();
+
+        private void onDavaAvailable(object sender, WaveInEventArgs e)
+        {
+            OnDataAvailable((WasapiLoopbackCapture)sender, e);
+        }
+
+        public virtual void OnDataAvailable(WasapiLoopbackCapture capture, WaveInEventArgs e)
+        {
+        }
+
+        public void Dispose()
+        {
+            waveIn.StopRecording();
+            waveIn.Dispose();
+        }
+    }
+}
--- RemoteAudioServer/MainWindow.cs
+++ RemoteAudioServer/MainWindow.cs
@@ -1,5 +1,7 @@
 namespace RemoteAudioServer;
 
+using RemoteAudioServer.Audio;
+
 public partial class MainWindow : Form
 {
     public MainWindow()
@@ -9,6 +11,9 @@ public partial class MainWindow : Form
 
         ToolTip toolTip = new ToolTip();
         toolTip.SetToolTip(DeviceInfo, "클라이언트에서 컴퓨터를 구분할때 사용합니다.");
+
+        using (AudioCapturer capturer = new AudioCapturer())
+            capturer.Start();
     }
 
     private void label1_Click(object sender, EventArgs e)
