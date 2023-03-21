78b49305a2b5fcf0039eb44d512419922517f713
Junho <51792037+ojh050118@users.noreply.github.com>
2023-03-21 12:13:13 +0900

    클라이언트 프로젝트 추가

---
+++ RemoteAudio.Client.Windows/MainWindow.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RemoteAudio.Client.Windows
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DeviceInfo = new System.Windows.Forms.Label();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(679, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(317, 428);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusText});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 451);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(1008, 22);
+            this.StatusStrip.TabIndex = 2;
+            this.StatusStrip.Text = "statusStrip1";
+            // 
+            // StatusText
+            // 
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(71, 17);
+            this.StatusText.Text = "연결 준비됨";
+            // 
+            // DeviceInfo
+            // 
+            this.DeviceInfo.AutoSize = true;
+            this.DeviceInfo.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DeviceInfo.Location = new System.Drawing.Point(12, 44);
+            this.DeviceInfo.Name = "DeviceInfo";
+            this.DeviceInfo.Size = new System.Drawing.Size(104, 32);
+            this.DeviceInfo.TabIndex = 3;
+            this.DeviceInfo.Text = "PC 정보:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 473);
+            this.Controls.Add(this.DeviceInfo);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainWindow";
+            this.Text = "Remote Audio Client";
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Button button1;
+        private StatusStrip StatusStrip;
+        private ToolStripStatusLabel StatusText;
+        private Label DeviceInfo;
+    }
+}
