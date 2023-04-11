71845a2069caca1b111d533714813535d15d6d0c
Junho <51792037+ojh050118@users.noreply.github.com>
2023-04-11 23:56:45 +0900

    `BroadcastingController` 추상화

---
+++ RemoteAudio.Core/Networking/BroadcastingController.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RemoteAudio.Core.Networking
+{
+    public abstract class BroadcastingController<T> : UdpClient where T : class
+    {
+        public int Port { get; }
+
+        public T Data;
+        public List<T> DataList { get; }
+        public event Action<T> DataReceived;
+
+        private CancellationTokenSource cancellationTokenSource;
+
+        public BroadcastingController(int port, T data)
+            : base(port)
+        {
+            EnableBroadcast = true;
+
+            cancellationTokenSource = new CancellationTokenSource();
+            DataList = new List<T>();
+
+            Data = data;
+            Port = port;
+        }
+
+        public virtual void Brodcast()
+        {
+            string info = JsonConvert.SerializeObject(Data);
+            var data = Encoding.UTF8.GetBytes(info);
+
+            IPEndPoint broadcast = new IPEndPoint(IPAddress.Broadcast, Port);
+
+            Send(data, data.Length, broadcast);
+        }
+
+        public virtual void ReceiveBroadcast()
+        {
+            BeginReceive(receiveCallback, null);
+        }
+
+        public virtual void StopReceivingBroadcast()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        protected abstract bool ReceiveCallback(T data);
+
+        private void receiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, Port);
+                byte[] receivedBytes = EndReceive(ar, ref remoteIpEndPoint);
+                string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
+
+                T data = JsonConvert.DeserializeObject<T>(receivedMessage);
+
+                if (ReceiveCallback(data))
+                    DataReceived.Invoke(data);
+
+                BeginReceive(receiveCallback, null);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ RemoteAudio.Core/Networking/RemoteAudioBroadcastingController.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoteAudio.Core.Networking
+{
+    public class RemoteAudioBroadcastingController : BroadcastingController<HostInfo>
+    {
+        public ServiceMode TargetServiceMode { get; }
+        public HostInfo HostInfo { get; }
+
+        public RemoteAudioBroadcastingController(int port, HostInfo hostInfo, ServiceMode serviceMode)
+            : base(port, hostInfo)
+        {
+            HostInfo = hostInfo;
+            TargetServiceMode = serviceMode;
+        }
+
+        protected override bool ReceiveCallback(HostInfo data)
+        {
+            if (data.ServiceMode != TargetServiceMode)
+                return false;
+
+            if (!DataList.Any(h => h.Equals(data)))
+                DataList.Add(data);
+
+            return true;
+        }
+    }
+}
