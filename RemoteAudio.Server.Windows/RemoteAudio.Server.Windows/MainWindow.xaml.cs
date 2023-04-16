60c50eceebe9657bfb087f0320479c1a7f459af3
Junho <51792037+ojh050118@users.noreply.github.com>
2023-04-16 16:15:50 +0900

    타겟 플랫폼 버전 업데이트

--- RemoteAudio.Server.Windows/RemoteAudio.Server.Windows/MainWindow.xaml.cs
+++ RemoteAudio.Server.Windows/RemoteAudio.Server.Windows/MainWindow.xaml.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
-using System;
-using Windows.ApplicationModel;
-using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
