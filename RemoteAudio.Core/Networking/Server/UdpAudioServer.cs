dc6387dca296c11f8e4480b3a0a5c178e233b0f3
Junho <51792037+ojh050118@users.noreply.github.com>
2023-04-17 23:03:59 +0900

    서버와 관련된 클래스를 `RemoteAudio.Core`로 이동

---
+++ RemoteAudio.Core/Networking/Server/UdpAudioServer.cs
@@ -0,0 +1,46 @@
+﻿using NAudio.Wave;
+using RemoteAudio.Core.Audio.Windows;
+using RemoteAudio.Core.Utils;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RemoteAudio.Core.Networking.Server
+{
+    public class UdpAudioServer : AudioCapturer
+    {
+        private UdpClient server;
+        private int port;
+
+        private RTPPacketData data;
+        private readonly Random random;
+
+        public IPAddress MulticastIPAddress { get; }
+
+        public UdpAudioServer(int port)
+        {
+            MulticastIPAddress = NetworkUtil.GetRandomMulticastAddress();
+            server = new UdpClient(port);
+            server.JoinMulticastGroup(MulticastIPAddress);
+            server.Ttl = 1;
+
+            data = new RTPPacketData();
+            random = new Random();
+
+            this.port = port;
+        }
+
+        protected override void OnDataAvailable(WasapiLoopbackCapture capture, WaveInEventArgs e)
+        {
+            base.OnDataAvailable(capture, e);
+
+            sendAudio(e.Buffer);
+        }
+
+        private void sendAudio(byte[] audioData)
+        {
+            data = server.SendRtpPacket(audioData, 0, data, new IPEndPoint(MulticastIPAddress, port));
+            data.SequenceNumber++;
+            data.Ssrc = (uint)random.NextInt64(0, uint.MaxValue);
+        }
+    }
+}
